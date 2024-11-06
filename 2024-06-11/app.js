f0049c316e81cd9d0f80e3557406d56a5e45caf4
shehanminura <shehanminuradp@gmail.com>
2024-11-06 11:28:22 +0530

    stap 2

--- 2024-06-11/app.js
+++ 2024-06-11/app.js
@@ -26,36 +26,82 @@
     
 // });
 
-let customer1 = {
-    name:"saman",
-    age:20,
-    address:"gamapaha",
-    items:[
-        {
-            itemCode:"C001",
-            qty:5
-        },
-        {
-            itemCode:"C002",
-            qty:[10,20,30,40,50,60]
-        },
-        {
-            itemCode:"C003",
-            qty:6
-        },
-        [
-            {
-                number:2
-            },{
-                number:5
-            },{
-                number:6
-            },{
-                number:9
-            },{
-                number:8
-            }
-        ]
-    ]
-};
+// let customer1 = {
+//     name:"saman",
+//     age:20,
+//     address:"gamapaha",
+//     items:[
+//         {
+//             itemCode:"C001",
+//             qty:5
+//         },
+//         {
+//             itemCode:"C002",
+//             qty:[10,20,30,40,50,60]
+//         },
+//         {
+//             itemCode:"C003",
+//             qty:6
+//         },
+//         [
+//             {
+//                 number:2
+//             },{
+//                 number:5
+//             },{
+//                 number:6
+//             },{
+//                 number:9
+//             },{
+//                 number:8
+//             }
+//         ]
+//     ]
+// };
+
+let customer={
+    name:"nimal",
+    age:20
+}
+let numbers = [10,50,68,70,95,102,305,150,72];
+
+numbers.push("Hello Shehan");
+//console.log(numbers);
+
+numbers.push(customer);
+
+//console.log(numbers);
+//numbers.pop();
+//console.log(numbers);
+
+//console.log(numbers.length);
+
+
+//console.log(numbers.toString());
+//console.log(numbers.indexOf(102));
+numbers.shift();
+console.log(numbers); //remov 10
+numbers.shift();
+console.log(numbers); //remov 50
+
+//-----------------------------------------------------------------------
+// numbers.unshift("Hello")
+// numbers.unshift("Hello1")
+
+// let letters = ["B","K","M","A","C","N"];
+
+// console.log(numbers.find(10));
+
+// let numbers = [10, 50, 68, 70, 95, 102, 305, 150, 72];
+// let numbers2 = [8, 6, 8, 72];
+// let numbers3 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let newAr = numbers.concat(numbers2,numbers3);
+
+// console.log(newAr);
+
+
+
+
+
 
