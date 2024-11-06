e44e94f1ba82b109091cc99a15385c249d478234
shehanminura <shehanminuradp@gmail.com>
2024-11-06 11:00:08 +0530

    make array in object

---
+++ 2024-06-11/app.js
@@ -0,0 +1,61 @@
+// let customer ={
+//     name:"Saman",
+//     age:20,
+//     address:"gampaha",
+//     iteam:[
+//         {
+//             iteamCode:"C001",
+//             qty:5
+//         },
+//         {
+//             iteamCode:"C002",
+//             qty:2
+//         },
+//         {
+//             iteamCode:"C003",
+//             qty:6
+//         },
+//     ]
+// }
+
+// console.log(customer);
+// console.log(customer.iteam);
+
+// customer.iteam.forEach(customers => {
+//     console.log(customers.iteamCode);
+    
+// });
+
+let customer1 = {
+    name:"saman",
+    age:20,
+    address:"gamapaha",
+    items:[
+        {
+            itemCode:"C001",
+            qty:5
+        },
+        {
+            itemCode:"C002",
+            qty:[10,20,30,40,50,60]
+        },
+        {
+            itemCode:"C003",
+            qty:6
+        },
+        [
+            {
+                number:2
+            },{
+                number:5
+            },{
+                number:6
+            },{
+                number:9
+            },{
+                number:8
+            }
+        ]
+    ]
+};
+
